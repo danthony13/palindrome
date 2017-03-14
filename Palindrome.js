ea347d8cbb8eccee1915d8c4c637cf51661fbaa0
danthony13 <david.anthony1095@yahoo.com>
2017-03-13 22:55:20 -0400

    first commit

---
+++ Palindrome.js
@@ -0,0 +1,11 @@
+
+function palindrome(str) {
+  var cleanStr = str.replace(/[^0-9a-z]/gi, '');
+  var downCase = cleanStr.toLowerCase();
+  var splitDownCase = downCase.split("");
+    return true;
+  }
+
+
+
+palindrome("almostomla");
